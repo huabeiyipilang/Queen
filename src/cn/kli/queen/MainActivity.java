c25a6c0a19fed3df3d45c734d9dd16c77bd54c51
kli <kli@ingenic.cn>
2013-01-16 16:28:47 +0800

    for updater

--- src/cn/kli/queen/MainActivity.java
+++ src/cn/kli/queen/MainActivity.java
@@ -1,14 +1,18 @@
 package cn.kli.queen;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 import cn.kli.launcher.TagGridView;
+import cn.kli.queen.updater.CheckUpdateActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 	private final static int TAG_COUNT = 4;
 	private final static int MSG_ADD_VIEW = 1; 
 	private LinearLayout mContainer;
@@ -21,6 +25,7 @@ public class MainActivity extends Activity {
 		mContainer = (LinearLayout)findViewById(R.id.container);
 		mGridView = new TagGridView(this);
 		mContainer.addView(mGridView);
+		findViewById(R.id.updater).setOnClickListener(this);
 	}
 	
 	private LayoutParams getTagViewParams(){
@@ -43,6 +48,15 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch(view.getId()){
+		case R.id.updater:
+			startActivity(new Intent(this, CheckUpdateActivity.class));
+			break;
+		}
 	}
 
 }
--- src/cn/kli/queen/QueenAppsManager.java
+++ src/cn/kli/queen/QueenAppsManager.java
@@ -3,6 +3,7 @@ package cn.kli.queen;
 import java.util.LinkedList;
 import java.util.List;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -36,6 +37,24 @@ public class QueenAppsManager {
 			}
 		}
 		return queen_apps;
+	}
+	
+	public void removeChildrenIcon(){
+		PackageManager pm = mContext.getPackageManager();
+        Intent intent = new Intent(Intent.ACTION_MAIN, null); 
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> apps = pm.queryIntentActivities(intent, 0);
+		for (ResolveInfo app : apps) {
+			if(app.activityInfo.packageName.startsWith("cn.kli.queen.")){
+				String pkg = app.activityInfo.packageName;
+				String cls = app.activityInfo.name;
+				ComponentName cName = new ComponentName(pkg,cls);
+				int state = false ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+				 : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+				pm.setComponentEnabledSetting(cName,state, PackageManager.DONT_KILL_APP);
+			}
+		}
+		
 	}
 	
 }
